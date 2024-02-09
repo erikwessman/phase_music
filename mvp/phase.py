12b1d9754fc54c366bec0fc5344cfe3eeffe4a38
theowiik <42991003+theowiik@users.noreply.github.com>
2024-02-09 23:49:39 +0100

    Add music transition and background fading

--- mvp/phase.py
+++ mvp/phase.py
@@ -20,7 +20,7 @@ class Game:
         self.screen = pygame.display.set_mode(self.window_size)
         pygame.display.set_caption("Phase Music")
 
-        # Music
+        # Music and Backgrounds
         self.phases = [
             Phase("Action", "audio/action.mp3", "img/action.png"),
             Phase("Encounter", "audio/encounter.mp3", "img/encounter.png"),
@@ -28,14 +28,13 @@ class Game:
         ]
         self.current_phase_index = 0
 
-        phase = self.phases[self.current_phase_index]
-        phase.sound.set_volume(1.0)
-        self.background = pygame.image.load(phase.img)
-        self.background = pygame.transform.scale(self.background, self.window_size)
-
     def run(self):
         running = True
-        self.phases[self.current_phase_index].sound.play(-1)
+        phase = self.phases[self.current_phase_index]
+        phase.sound.play(-1)
+        phase.sound.set_volume(1.0)
+        self.background = pygame.image.load(phase.img).convert()
+        self.background = pygame.transform.scale(self.background, self.window_size)
 
         while running:
             for event in pygame.event.get():
@@ -52,19 +51,33 @@ class Game:
         current_phase = self.phases[self.current_phase_index]
         next_phase_index = (self.current_phase_index + 1) % len(self.phases)
         next_phase = self.phases[next_phase_index]
-
         next_phase.sound.play(-1)
-        for vol in range(10, -1, -1):
-            current_phase.sound.set_volume(vol / 10.0)
-            next_phase.sound.set_volume((10 - vol) / 10.0)
-            pygame.time.delay(700)
+        next_phase.sound.set_volume(0.0)
+
+        # Capture current screen
+        current_screen = pygame.Surface(self.window_size).convert()
+        current_screen.blit(self.screen, (0, 0))
+
+        # Load next background
+        next_background = pygame.image.load(next_phase.img).convert()
+        next_background = pygame.transform.scale(next_background, self.window_size)
+
+        total_steps = 255
+        for step in range(total_steps + 1):
+            alpha = step * (255 // total_steps)
+            self.screen.blit(current_screen, (0, 0))
+            next_background.set_alpha(alpha)
+            self.screen.blit(next_background, (0, 0))
+            pygame.display.flip()
+            pygame.time.delay(100)
+
+            new_volume = alpha / 255.0
+            next_phase.sound.set_volume(new_volume)
+            current_phase.sound.set_volume(1.0 - new_volume)
 
         current_phase.sound.stop()
         self.current_phase_index = next_phase_index
-
-        # Background
-        self.background = pygame.image.load(next_phase.img)
-        self.background = pygame.transform.scale(self.background, self.window_size)
+        self.background = next_background
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))

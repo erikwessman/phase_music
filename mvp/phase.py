c8e4d53d0a4315041eb9ca2b14c7d366ff595592
theowiik <42991003+theowiik@users.noreply.github.com>
2024-02-09 23:31:45 +0100

    Next phase button

--- mvp/phase.py
+++ mvp/phase.py
@@ -14,34 +14,22 @@ class Game:
     def __init__(self):
         pygame.font.init()
         pygame.mixer.init()
-        screen_side = 640
-        button_width = 150
-        button_height = 50
-        button_spacing = 10  # Space between buttons
+        screen_size = 640
+        button_width = 100
+        button_height = 25
 
-        self.screen_size = (screen_side, screen_side)
-        self.screen = pygame.display.set_mode(self.screen_size)
+        self.window_size = (screen_size, screen_size)
+        self.screen = pygame.display.set_mode(self.window_size)
         pygame.display.set_caption("Phase Music")
 
         # UI
         self.font = pygame.font.Font(None, 24)
-        self.button_color = (171, 171, 171)
-        self.button_highlight_color = (255, 100, 100)
         self.next_phase_button = pygame.Rect(
-            (screen_side - button_width) / 2,
-            (screen_side + button_height) / 2 + button_spacing,
+            (screen_size - button_width) / 2,
+            (screen_size + button_height) / 2,
             button_width,
             button_height,
         )
-        self.current_phase_button = pygame.Rect(
-            (screen_side - button_width * 1.2) / 2,
-            (screen_side - button_height * 1.5) / 2 - button_spacing,
-            button_width * 1.2,
-            button_height * 1.2,
-        )
-        self.button_border_color = (100, 255, 255)
-        self.button_border_width = 2
-        self.current_phase_button_color = (25, 110, 55)
 
         # Music
         self.phases = [
@@ -54,7 +42,7 @@ class Game:
         phase = self.phases[self.current_phase_index]
         phase.sound.set_volume(1.0)
         self.background = pygame.image.load(phase.img)
-        self.background = pygame.transform.scale(self.background, self.screen_size)
+        self.background = pygame.transform.scale(self.background, self.window_size)
 
     def run(self):
         running = True
@@ -88,45 +76,20 @@ class Game:
 
         # Background
         self.background = pygame.image.load(next_phase.img)
-        self.background = pygame.transform.scale(self.background, self.screen_size)
+        self.background = pygame.transform.scale(self.background, self.window_size)
 
     def _draw(self):
         self.screen.blit(self.background, (0, 0))
 
-        # Next Phase Button
-        pygame.draw.rect(self.screen, self.button_color, self.next_phase_button)
-        pygame.draw.rect(
-            self.screen,
-            self.button_border_color,
-            self.next_phase_button,
-            self.button_border_width,
-        )
-
-        # Current Phase Button
-        pygame.draw.rect(
-            self.screen, self.current_phase_button_color, self.current_phase_button
-        )
-        pygame.draw.rect(
-            self.screen,
-            self.button_border_color,
-            self.current_phase_button,
-            self.button_border_width,
-        )
+        # Button background
+        pygame.draw.rect(self.screen, (38, 19, 79), self.next_phase_button)
 
         # Button text
-        next_phase_text = self.font.render("Next Phase", True, (0, 0, 0))
+        next_phase_text = self.font.render("Next Phase", True, (255, 255, 255))
         next_phase_text_rect = next_phase_text.get_rect(
             center=self.next_phase_button.center
         )
         self.screen.blit(next_phase_text, next_phase_text_rect.topleft)
-
-        current_phase_text = self.font.render(
-            self.phases[self.current_phase_index].name, True, (255, 255, 255)
-        )
-        current_phase_text_rect = current_phase_text.get_rect(
-            center=self.current_phase_button.center
-        )
-        self.screen.blit(current_phase_text, current_phase_text_rect.topleft)
 
         pygame.display.flip()
 
